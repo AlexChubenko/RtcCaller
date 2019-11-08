fe9f90d5d268b4e2454339a3bd28d2976289d68d
Oleksandr_Chubenko <logist.m6@gmail.com>
2019-11-08 17:17:30 +0200

    0001

---
+++ app/src/main/java/com/anuntis/rtccaller/displays/call/CallActivity.kt
@@ -0,0 +1,8 @@
+package com.anuntis.rtccaller.displays.call
+
+import androidx.appcompat.app.AppCompatActivity
+
+class CallActivity : AppCompatActivity() {
+
+
+}
---
+++ app/src/main/java/com/anuntis/rtccaller/displays/contacts/ContactsActivity.kt
@@ -0,0 +1,13 @@
+package com.anuntis.rtccaller.displays.contacts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.anuntis.rtccaller.R
+
+class ContactsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
